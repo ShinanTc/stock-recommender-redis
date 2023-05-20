82be51f428a213cad72dcaa551862a3837228357
Shinan Tc <shinantc@croswer.com>
2023-05-20 14:21:51 +0530

    going to URL and closing pop-up successfuull

--- src/services/scrapeStockData.js
+++ src/services/scrapeStockData.js
@@ -5,13 +5,16 @@ export async function scrapeStockData() {
 
     try {
         const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
+
+        // getting the first tab
+        const page = (await browser.pages())[0];
 
         // indiainfoline.com - for stock recommendation data
 
         await page.goto('https://www.indiainfoline.com/stock-ideas');
 
         setTimeout(async () => {
+
             console.log("Waiting for 12 seconds");
 
             // close the popup button
