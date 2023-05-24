b8734d89a9fda73ebbf9b063213d8d5e6fa71a26
Shinan Tc <shinantc@croswer.com>
2023-05-24 17:39:34 +0530

    Clicking 'Last' button is successfull

--- src/services/scrapeStockData.js
+++ src/services/scrapeStockData.js
@@ -1,11 +1,11 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Keyboard } from 'puppeteer';
 
 // go to the website
 export async function scrapeStockData() {
 
     try {
-        // const browser = await puppeteer.launch({ headless: true });
-        const browser = await puppeteer.launch({ headless: 'new' });
+        const browser = await puppeteer.launch({ headless: false });
+        // const browser = await puppeteer.launch({ headless: 'new' });
 
         // getting the first tab
         const page = (await browser.pages())[0];
@@ -27,33 +27,46 @@ export async function scrapeStockData() {
 
             console.log("Pop up closed");
 
+            // go to the very last page
+            await page.waitForSelector('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+            const element = await page.$('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+            await element.scrollIntoView();
+            await page.evaluate(el => el.click(), element);
+
+            console.log('Clicking "Last" button successfull');
 
 
-            let i = 1;
-            let stockTickerSymbolArr = [];
-
-            while (i < 20) {
-
-                let stockTickerSymbolXpath = await page.$x(`//*[@id="myGroup"]/tr[${i}]/td[1]/span[1]`); // Replace "//xpath/here" with your desired XPath expression
-                let stockTickerSymbol = await page.evaluate(el => el.textContent, stockTickerSymbolXpath[0]);
-
-                // getting the stock ticker symbol (Eg: TATAPOWER)
-                stockTickerSymbol = stockTickerSymbol.split(' ')[0].trim();
-
-                stockTickerSymbolArr.push(stockTickerSymbol);
-
-                i += 2;
-            }
 
 
-            console.log('stockTickerSymbolArr');
-            console.log(stockTickerSymbolArr);
 
-            // const stockTickerSymbolXpath = await page.$x('//*[@id="myGroup"]/tr[1]/td[1]/span[1]'); // Replace "//xpath/here" with your desired XPath expression
-            // let stockTickerSymbol = await page.evaluate(el => el.textContent, stockTickerSymbolXpath[0]);
 
-            // // getting the stock ticker symbol (Eg: TATAPOWER)
-            // stockTickerSymbol = stockTickerSymbol.split(' ')[0].trim();
+
+
+
+
+
+            // let i = 1;
+
+            // acting as a database
+            // let stockTickerSymbolArr = [];
+
+            // while (i < 20) {
+
+            //     // getting the stock ticker symbol (Eg: TATAPOWER)
+            //     let stockTickerSymbolXpath = await page.$x(`//*[@id="myGroup"]/tr[${i}]/td[1]/span[1]`); // Replace "//xpath/here" with your desired XPath expression
+            //     let stockTickerSymbol = await page.evaluate(el => el.textContent, stockTickerSymbolXpath[0]);
+
+            //     stockTickerSymbol = stockTickerSymbol.split(' ')[0].trim();
+
+            //     stockTickerSymbolArr.push(stockTickerSymbol);
+
+            //     i += 2;
+
+            // }
+
+
+            // console.log('stockTickerSymbolArr');
+            // console.log(stockTickerSymbolArr);
 
         }, 12000);
 
