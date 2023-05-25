df7173ce4b534dda24845e61069d91f1e696595c
Shinan Tc <shinantc@croswer.com>
2023-05-25 14:27:23 +0530

    Going back to the first page successfull

--- src/services/scrapeStockData.js
+++ src/services/scrapeStockData.js
@@ -4,8 +4,8 @@ import puppeteer, { Keyboard } from 'puppeteer';
 export async function scrapeStockData() {
 
     try {
-        // const browser = await puppeteer.launch({ headless: false });
-        const browser = await puppeteer.launch({ headless: 'new' });
+        const browser = await puppeteer.launch({ headless: false });
+        // const browser = await puppeteer.launch({ headless: 'new' });
 
         // getting the first tab
         const page = (await browser.pages())[0];
@@ -28,27 +28,41 @@ export async function scrapeStockData() {
             console.log("Pop up closed");
 
 
-            // go to the very last page
-            await page.waitForSelector('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
-            const element = await page.$('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
-            await element.scrollIntoView();
-            await page.evaluate(el => el.click(), element);
+            // // go to the very last page
+            // await page.waitForSelector('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+            // const lastPage = await page.$('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+            // await lastPage.scrollIntoView();
+            // await page.evaluate(el => el.click(), lastPage);
 
-            console.log('Clicking "Last" button successfull');
+            // console.log('Clicking "Last" button successfull');
 
 
             // get the total no of pages
             await page.waitForXPath('/html/body/app-root/div/app-stock-view-details/div/div/div[1]/div/div/div[3]/div/div/app-pagination/div/ngb-pagination/ul/li[4]/a');
-            const elt = await page.$x('/html/body/app-root/div/app-stock-view-details/div/div/div[1]/div/div/div[3]/div/div/app-pagination/div/ngb-pagination/ul/li[4]/a');
-            let totalNumberOfPages = await page.evaluate(el => el.textContent, elt[0]);
+            const numberOfPagesXpath = await page.$x('/html/body/app-root/div/app-stock-view-details/div/div/div[1]/div/div/div[3]/div/div/app-pagination/div/ngb-pagination/ul/li[4]/a');
+            let totalNumberOfPages = await page.evaluate(el => el.textContent, numberOfPagesXpath[0]);
+
+            // this means then the xpath of elements may have changed,
+            // because totalNumberOfPages variable should have a number
+            if (isNumber(totalNumberOfPages) === false) {
+                console.error("ERROR : This is not a number");
+            }
 
             // getting only the number from the string
-            totalNumberOfPages = totalNumberOfPages.split(' ')[1].trim();  // trim - for removing whitespaces
+            totalNumberOfPages = totalNumberOfPages.split(' ')[1]?.trim();
 
             // converting string to integer
             totalNumberOfPages = parseInt(totalNumberOfPages);
 
-            console.log("Gathered total no of pages");
+            console.log("Gathered total no of pages : " + totalNumberOfPages);
+
+            // go back to the first page
+            await page.waitForSelector('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(1) > a');
+            const firstPage = await page.$('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(1) > a');
+            await firstPage.scrollIntoView();
+            await page.evaluate(el => el.click(), firstPage);
+
+            console.log('Going to the first page successfull');
 
 
 
@@ -77,12 +91,13 @@ export async function scrapeStockData() {
 
         }, 12000);
 
-        // xpath of stock names
-        //*[@id="myGroup"]/tr[1]/td[1]/span[1]
-        //*[@id="myGroup"]/tr[3]/td[1]/span[1]
-        //*[@id="myGroup"]/tr[5]/td[1]/span[1]
-        // ...
-        //*[@id="myGroup"]/tr[19]/td[1]/span[1]
+        // go to the very last page
+        await page.waitForSelector('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+        const lastPage = await page.$('body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(9) > a');
+        await lastPage.scrollIntoView();
+        await page.evaluate(el => el.click(), lastPage);
+
+        console.log('Clicking "Last" button successfull');
 
     } catch (error) {
         throw error;
@@ -90,3 +105,20 @@ export async function scrapeStockData() {
 }
 
 scrapeStockData();
+
+// check whether a given input is a number or not (not datatypes)
+// this function will even take '42' and 40 as numbers
+function isNumber(value) {
+    return typeof value === 'number' || !isNaN(parseInt(value));
+}
+
+
+
+
+
+// xpath of stock names
+//*[@id="myGroup"]/tr[1]/td[1]/span[1]
+//*[@id="myGroup"]/tr[3]/td[1]/span[1]
+//*[@id="myGroup"]/tr[5]/td[1]/span[1]
+// ...
+//*[@id="myGroup"]/tr[19]/td[1]/span[1]
