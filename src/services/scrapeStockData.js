c05817b89573f7952265c938254c8b8827e90b3e
Shinan Tc <shinantc@croswer.com>
2023-05-28 13:44:15 +0530

    getting stock data successfull

--- src/services/scrapeStockData.js
+++ src/services/scrapeStockData.js
@@ -5,8 +5,8 @@ export async function scrapeStockData() {
 
     try {
 
-        // const browser = await puppeteer.launch({ headless: false });
-        const browser = await puppeteer.launch({ headless: 'new' });
+        const browser = await puppeteer.launch({ headless: false });
+        // const browser = await puppeteer.launch({ headless: 'new' });
 
         // getting the first tab
         const page = (await browser.pages())[0];
@@ -31,40 +31,47 @@ export async function scrapeStockData() {
             let i = 1; // Start with the initial value for `i`
 
             // going throught the stock data in each page
-            while (true) {
+            while (i < 10) {
 
-                let element = await waitForXPathAndReturnElements(page, `//*[@id="myGroup"]/tr[${i}]/td[1]/span[1]`);
+                let xPaths = [
+                    // stockTickerNameXpath
+                    `//*[@id="myGroup"]/tr[${i}]/td[1]/span[1]`,
+                    // ltp
+                    `//*[@id="myGroup"]/tr[${i}]/td[3]`,
+                    // target
+                    `//*[@id="myGroup"]/tr[${i}]/td[4]/div/span[1]`,
+                    // profit percentage
+                    `//*[@id="myGroup"]/tr[${i}]/td[4]/div/span[2]`
+                ];
+
+                let element = await waitForXPathAndReturnElements(page, xPaths);
 
                 // getting the stock ticker name
-                let stockTickerName = await getStockTickerName(page, element);
-
-                console.log('stockTickerName');
-                console.log(stockTickerName);
-
+                let stockDetails = await getStockDetails(page, element);
 
                 // if the function returned a timout error (took too long to respond)
-                if (element === 'TimeoutError') {
+                // if (element === 'TimeoutError') {
 
-                    let returnValue = await clickNext(page, 'body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(8) > a');
+                //     let returnValue = await clickNext(page, 'body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(8) > a');
 
-                    // If clickNext times out, it could be due to a network issue or reaching the page before the last page.
-                    // To handle this, we change the selector for the next button on the last page.
-                    if (returnValue === 'TimeoutError') {
+                //     // If clickNext times out, it could be due to a network issue or reaching the page before the last page.
+                //     // To handle this, we change the selector for the next button on the last page.
+                //     if (returnValue === 'TimeoutError') {
 
-                        // When we reach the page before the last page, the selector of the Next button changes.
-                        // To handle this, we call the clickNext function again with a different selector.
-                        let returnValue = await clickNext(page, 'body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(5) > a');
+                //         // When we reach the page before the last page, the selector of the Next button changes.
+                //         // To handle this, we call the clickNext function again with a different selector.
+                //         let returnValue = await clickNext(page, 'body > app-root > div > app-stock-view-details > div > div > div.col-lg-8 > div > div > div.row.stock-table-MT.ng-star-inserted > div > div > app-pagination > div > ngb-pagination > ul > li:nth-child(5) > a');
 
-                        // If this returns a 'TimeoutError', it indicates that we have reached the last page.
-                        if (returnValue === 'TimeoutError')
-                            break;
+                //         // If this returns a 'TimeoutError', it indicates that we have reached the last page.
+                //         if (returnValue === 'TimeoutError')
+                //             break;
 
-                    }
+                //     }
 
-                } else {
-                    numberOfStocks++;
-                    i += 2; // Increment 'i' to move to the next row
-                }
+                // } else {
+                //     numberOfStocks++;
+                //     i += 2; // Increment 'i' to move to the next row
+                // }
 
             }
 
@@ -82,12 +89,18 @@ scrapeStockData();
 
 // async function getDataByXpath(page, xPath) { }
 
-async function waitForXPathAndReturnElements(page, xPath) {
+async function waitForXPathAndReturnElements(page, xPaths) {
 
     try {
-        await page.waitForXPath(xPath);
-        const elements = await page.$x(xPath);
-        return elements;
+        // await page.waitForXPath(xPath);
+        // const elements = await page.$x(xPath);
+        // return elements;
+
+        const promises = xPaths.map(xpath => page.waitForXPath(xpath));
+        const element = await Promise.all(promises);
+
+        return element;
+
     } catch (error) {
         // Handle timeout error
         if (error.name === 'TimeoutError')
@@ -119,12 +132,18 @@ async function clickNext(page, selector) {
 }
 
 // get stock ticker name
-async function getStockTickerName(page, element) {
+async function getStockDetails(page, element) {
     let stockTickerName = await page.evaluate(el => el.textContent, element[0]);
+    let ltp = await page.evaluate(el => el.textContent, element[1]);
+    let target = await page.evaluate(el => el.textContent, element[2]);
+    let profitPercentage = await page.evaluate(el => el.textContent, element[3]);
 
-    // 'stockTickerName' will be like this : TATAPOWER | NSE
-    // so we need to extract the name only
-    stockTickerName = stockTickerName.split(' ')[0]?.trim();
+    // 'stockTickerName' will be like this : TATAPOWER | NSE, so we need to extract the name only
+    // log each variable for better understanding
+    stockTickerName = await stockTickerName.split(' ')[0]?.trim();
+    ltp = await ltp.split(' ')[2]?.trim();
+    target = await target.trim();
+    profitPercentage = await profitPercentage.replace(/[()]/g, '').trim();
 
-    return stockTickerName;
+    return { stockTickerName, ltp, target, profitPercentage };
 }
