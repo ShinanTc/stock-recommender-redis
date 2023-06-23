7f6e50c463ae3a8435b0eb2a09baed0b032c81ab
shinan <shinantc1@gmail.com>
2023-06-23 12:27:21 +0530

    Function created for removing NaN values

--- src/helpers/processScrapedData.js
+++ src/helpers/processScrapedData.js
@@ -1,1 +1,13 @@
 // process the scraped data based on the user's affordability and similar stuff
+
+// remove stockdatas which has NaN values in it
+async function removeNaNvalues(stockData) {
+    stockData = stockData.filter((data) => {
+        const values = data.split('|');
+        return values.every((value) => !isNaN(value));
+    });
+
+    return stockData;
+}
+
+removeNaNvalues();
