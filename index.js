6170ed7f06dcd4c6c0df897887554b01e6877b8b
shinan <shinantc1@gmail.com>
2023-08-10 13:52:30 +0530

    commented redis client connection establishment from index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { startServer } from "./src/config/server/server-config.js";
 import middlewares from "./src/middlewares/utils/middlewares.js";
 import routeIndex from "./src/routes/routeIndex.js";
 import dotenv from "dotenv";
-import { getClient } from "./src/config/db/redis-config.js";
+// import { getClient } from "./src/config/db/redis-config.js";
 import { scheduleCronJob } from "./src/services/scheduleScrape.js";
 
 const app = express();
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(middlewares);
 
 // connect to redis
-getClient();
+// getClient();
 
 // entry to all routes
 app.use("/", routeIndex);
