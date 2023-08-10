be2e2e216752ea3fbe27b57381230796c58e9e43
shinan <shinantc1@gmail.com>
2023-08-10 19:57:11 +0530

    redis configuration modified

--- src/config/db/redis-config.js
+++ src/config/db/redis-config.js
@@ -14,26 +14,29 @@ export function getClient() {
       password: process.env.REDIS_PASSWORD,
     });
 
+    console.log('client');
+    console.log(client);
+
     // on error event
-    client.on("error", (err) => {
-      console.error("Redis Client Error", err);
-      throw err;
-    });
+    // client.on("error", (err) => {
+    //   console.error("Redis Client Error", err);
+    //   throw err;
+    // });
 
-    // connect to redis
-    client.connect((err) => {
-      if (err) {
-        console.error("Failed to connect to Redis", err);
-        throw err;
-      } else {
-        console.log("Redis connection succesfull!!!");
-        throw err;
-      }
-    });
+    // // connect to redis
+    // client.connect((err) => {
+    //   if (err) {
+    //     console.error("Failed to connect to Redis", err);
+    //     throw err;
+    //   } else {
+    //     console.log("Redis connection succesfull!!!");
+    //     throw err;
+    //   }
+    // });
 
-    client.on("connect", () => {
-      console.log("Redis connection Established");
-    });
+    // client.on("connect", () => {
+    //   console.log("Redis connection Established");
+    // });
   }
 
   return client;
