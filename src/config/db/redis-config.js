1eb16f1f89b810a9eae133cf27a04432b2dd06db
shinan <shinantc1@gmail.com>
2023-08-17 12:53:51 +0530

    redis-config.js file resetted to old format

--- src/config/db/redis-config.js
+++ src/config/db/redis-config.js
@@ -1,8 +1,4 @@
-// import { createClient } from "redis";
-import { createClient, kv } from '@vercel/kv';
-import dotenv from "dotenv";
-
-dotenv.config();
+import { createClient } from "redis";
 
 let client = null;
 
@@ -10,49 +6,28 @@ export async function getClient() {
   if (!client) {
 
     // development
-    // client = createClient({
-    //   host: process.env.REDIS_HOST,
-    //   port: process.env.REDIS_PORT,
-    //   password: process.env.REDIS_PASSWORD
-    // });
+    client = createClient();
     
-    // earlier production
-    // client = createClient({
-    //   host: process.env.process.env.KV_URL,
-    //   // port: process.env.REDIS_PORT,
-    //   port: 6379,  // default redis port
-    //   password: process.env.KV_REST_API_TOKEN
-    // });
-
-    // current production
-    client = createClient({
-      url: process.env.KV_URL,
-      token: process.env.KV_REST_API_TOKEN
+    // on error event
+    client.on("error", (err) => {
+      console.error("Redis Client Error", err);
+      throw err;
     });
 
-    // await kv.set('TATAPOWER','13123123123123');
-    // const getExample = await kv.get('TATAPOWER');
-  
-    // on error event
-    // client.on("error", (err) => {
-    //   console.error("Redis Client Error", err);
-    //   throw err;
-    // });
+    // connect to redis
+    client.connect((err) => {
+      if (err) {
+        console.error("Failed to connect to Redis", err);
+        throw err;
+      } else {
+        console.log("Redis connection succesfull!!!");
+        throw err;
+      }
+    });
 
-    // // connect to redis
-    // client.connect((err) => {
-    //   if (err) {
-    //     console.error("Failed to connect to Redis", err);
-    //     throw err;
-    //   } else {
-    //     console.log("Redis connection succesfull!!!");
-    //     throw err;
-    //   }
-    // });
-
-    // client.on("connect", () => {
-    //   console.log("Redis connection Established");
-    // });
+    client.on("connect", () => {
+      console.log("Redis connection Established");
+    });
 
   }
 
