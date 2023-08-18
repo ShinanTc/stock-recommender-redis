09739083a50f2efe25d5000773e7736820c2cbcb
shinan <shinantc1@gmail.com>
2023-08-18 14:46:44 +0530

    vercel.json and scheduleScrape.js modified

--- src/services/scheduleScrape.js
+++ src/services/scheduleScrape.js
@@ -4,8 +4,8 @@ import { createStockData } from "../helpers/db/redis-db-helper.js";
 export const scheduleCronJob = async () => {
   console.log("scheduling cron job");
 
-  // Schedule the cron job to run at 11:55 am every day in the 'Asia/Kolkata' time zone
-  cron.schedule("23 13 * * *", createStockData, {
+  // Schedule the cron job to run at 10:00 am every day in the 'Asia/Kolkata' time zone
+  cron.schedule("0 10 * * *", createStockData, {
     timezone: "Asia/Kolkata",
   });
 };
