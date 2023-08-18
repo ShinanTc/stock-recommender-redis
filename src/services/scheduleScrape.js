6b05aad4d3f7f1483f28c6f2387110a931b5ffc5
shinan <shinantc1@gmail.com>
2023-08-18 11:30:45 +0530

    time corrected in scheduleCronJob.js file

--- src/services/scheduleScrape.js
+++ src/services/scheduleScrape.js
@@ -4,8 +4,8 @@ import { createStockData } from "../helpers/db/redis-db-helper.js";
 export const scheduleCronJob = async () => {
   console.log("scheduling cron job");
 
-  // Schedule the cron job to run at 11:37 am every day in the 'Asia/Kolkata' time zone
-  cron.schedule("00 10 * * *", createStockData, {
+  // Schedule the cron job to run at 12:00 pm every day in the 'Asia/Kolkata' time zone
+  cron.schedule("0 12 * * *", createStockData, {
     timezone: "Asia/Kolkata",
   });
 };
