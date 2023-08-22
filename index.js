dbad7b812299a1fad55c51e416176877353915ae
shinan <shinantc1@gmail.com>
2023-08-22 13:11:45 +0530

    redis-config.js file removed

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { startServer } from "./src/config/server/server-config.js";
 import middlewares from "./src/middlewares/utils/middlewares.js";
 import routeIndex from "./src/routes/routeIndex.js";
 import dotenv from "dotenv";
-import { createStockData } from "./src/helpers/db/redis-db-helper.js"; // Assuming you have a separate file for createStockData.
 import { scheduleCronJob } from "./src/services/scrapeStockData.js";
 import { watchVariable } from "./src/services/variableWatcher.js";
 
@@ -16,9 +15,6 @@ dotenv.config();
 app.use(middlewares);
 
 global.cronJobCompleted = false;
-
-// connect to redis
-// getClient();
 
 // entry to all routes
 app.use("/", routeIndex);
