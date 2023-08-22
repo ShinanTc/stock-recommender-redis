6caddfab1c26b94b744e91b34bccff344b438ad4
shinan <shinantc1@gmail.com>
2023-08-22 18:03:42 +0530

    automated cron jobs temporarily disabled for testing

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { startServer } from "./src/config/server/server-config.js";
 import middlewares from "./src/middlewares/utils/middlewares.js";
 import routeIndex from "./src/routes/routeIndex.js";
 import dotenv from "dotenv";
-import { scheduleCronJob } from "./src/services/scrapeStockData.js";
-import { watchVariable } from "./src/services/variableWatcher.js";
+// import { scheduleCronJob } from "./src/services/scrapeStockData.js";
+// import { watchVariable } from "./src/services/variableWatcher.js";
 
 const app = express();
 
@@ -22,9 +22,9 @@ app.use("/", routeIndex);
 // Start the server
 startServer(app);
 
-(async () => {
-  await scheduleCronJob(); // Wait for the cron job to complete
+// (async () => {
+//   await scheduleCronJob(); // Wait for the cron job to complete
 
-  // Check ABC at regular intervals (e.g., every second)
-  setInterval(watchVariable, 3600000);
-})();
+//   // Check ABC at regular intervals (e.g., every second)
+//   setInterval(watchVariable, 3600000);
+// })();
