befad723bb73c655a1182ace9f3f3a1729197614
shinan <shinantc1@gmail.com>
2023-08-23 12:06:43 +0530

    vercel.json file modified

---
+++ api/cron.js
@@ -0,0 +1,14 @@
+export default async function handler(request, response) {
+  try {
+    // Your cron job logic goes here
+    // This block will run according to the schedule specified in vercel.json
+    // Replace this comment with your cron job code
+    console.log("Cron job is working (FROM HANDLER");
+
+    // Respond with a success message
+    response.status(200).send("Cron job executed successfully.");
+  } catch (error) {
+    console.error("Error:", error);
+    response.status(500).send("Internal Server Error");
+  }
+}
--- api/serverless-example/handler.js
+++
@@ -1,9 +0,0 @@
-export default function handler(request, response) {
-  console.log("Cron job is working (FROM HANDLER)");
-
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
-}
