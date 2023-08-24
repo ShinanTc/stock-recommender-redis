ce40919ea1eca848802e43e4a617844c903ef245
shinan <shinantc1@gmail.com>
2023-08-24 20:08:03 +0530

    schedule time inside vercel.json modified

--- api/cron.js
+++ api/cron.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
   console.log("Cron job successfull (FROM HANDLER");
 
     // Respond with a success message
-    response.status(200).send("Cron job executed successfully.");
+    response.status(200).send("Cron job executed successfully");
   } catch (error) {
     console.error("Error:", error);
     response.status(500).send("Internal Server Error");
