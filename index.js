84d8cba17a5076dde9b05f53893c17c53b94d53e
shinan <shinantc1@gmail.com>
2023-08-24 13:15:11 +0530

    setInterval method used for testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { startServer } from "./src/config/server/server-config.js";
 import middlewares from "./src/middlewares/utils/middlewares.js";
 import routeIndex from "./src/routes/routeIndex.js";
 import dotenv from "dotenv";
+import { createStockData } from "./src/helpers/db/redis-db-helper.js";
 // import { scheduleCronJob } from "./src/services/scrapeStockData.js";
 // import { watchVariable } from "./src/services/variableWatcher.js";
 
@@ -22,9 +23,4 @@ app.use("/", routeIndex);
 // Start the server
 startServer(app);
 
-// (async () => {
-//   await scheduleCronJob(); // Wait for the cron job to complete
-
-//   // Check ABC at regular intervals (e.g., every second)
-//   setInterval(watchVariable, 3600000);
-// })();
+setInterval(() => createStockData(), 1200000);
